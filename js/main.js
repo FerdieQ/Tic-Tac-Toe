75642aca54bb4001a20a2113b0f03ee6b8cc2110
Ferdie Quizon <fquizon@gmail.com>
2016-01-06 00:37:10 -0800

    renamed project folder and added semicolons

--- js/main.js
+++ js/main.js
@@ -11,74 +11,73 @@ function clickBox(box) {
     document.getElementById(box).disabled = "disabled";
     player +=1;
     winner();
- }
-}
+ };
+};
 
 function winner (){
     if(document.getElementById("box1").value === "X" &&
         document.getElementById("box2").value === "X" &&
-        document.getElementById("box3").value === "X") 
+        document.getElementById("box3").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box4").value === "X" &&
         document.getElementById("box5").value === "X" &&
-        document.getElementById("box6").value === "X") 
+        document.getElementById("box6").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box7").value === "X" &&
         document.getElementById("box8").value === "X" &&
-        document.getElementById("box9").value === "X") 
+        document.getElementById("box9").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box1").value === "X" &&
         document.getElementById("box4").value === "X" &&
-        document.getElementById("box7").value === "X") 
+        document.getElementById("box7").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box2").value === "X" &&
         document.getElementById("box5").value === "X" &&
-        document.getElementById("box8").value === "X") 
+        document.getElementById("box8").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box3").value === "X" &&
         document.getElementById("box6").value === "X" &&
-        document.getElementById("box9").value === "X") 
+        document.getElementById("box9").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box1").value === "X" &&
         document.getElementById("box5").value === "X" &&
-        document.getElementById("box9").value === "X") 
+        document.getElementById("box9").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box3").value === "X" &&
         document.getElementById("box5").value === "X" &&
-        document.getElementById("box7").value === "X") 
+        document.getElementById("box7").value === "X")
         {alert("Player 1 WINS!");
     } if(document.getElementById("box1").value === "O" &&
         document.getElementById("box2").value === "O" &&
-        document.getElementById("box3").value === "O") 
+        document.getElementById("box3").value === "O")
         {alert("Player 2 WINS!");
     } if(document.getElementById("box4").value === "O" &&
         document.getElementById("box5").value === "O" &&
-        document.getElementById("box6").value === "O") 
+        document.getElementById("box6").value === "O")
         {alert("Player 2 WINS!");
     } if(document.getElementById("box7").value === "O" &&
         document.getElementById("box8").value === "O" &&
-        document.getElementById("box9").value === "O") 
+        document.getElementById("box9").value === "O")
         {alert("Player 2 WINS!");
     } if(document.getElementById("box1").value === "O" &&
         document.getElementById("box4").value === "O" &&
-        document.getElementById("box7").value === "O") 
+        document.getElementById("box7").value === "O")
         {alert("Player 2 WINS!");
     } if(document.getElementById("box2").value === "O" &&
         document.getElementById("box5").value === "O" &&
-        document.getElementById("box8").value === "O") 
+        document.getElementById("box8").value === "O")
         {alert("Player 2 WINS!");
     } if(document.getElementById("box3").value === "O" &&
         document.getElementById("box6").value === "O" &&
-        document.getElementById("box9").value === "O") 
+        document.getElementById("box9").value === "O")
         {alert("Player 2 WINS!");
     } if(document.getElementById("box1").value === "O" &&
         document.getElementById("box5").value === "O" &&
-        document.getElementById("box9").value === "O") 
+        document.getElementById("box9").value === "O")
         {alert("Player 2 WINS!");
     } if(document.getElementById("box3").value === "O" &&
         document.getElementById("box5").value === "O" &&
-        document.getElementById("box7").value === "O") 
+        document.getElementById("box7").value === "O")
         {alert("Player 2 WINS!");
-    }
-
-}
+    };
+};
