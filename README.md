bb9893ef6a390c4a6f3ebcfd1f6d4e765a7501c4
Ferdie Quizon <fquizon@gmail.com>
2016-01-12 23:10:30 -0800

    cloned master

---
+++ README.md
@@ -0,0 +1,27 @@
+#![alt text](https://raw.githubusercontent.com/FerdieQ/Tic_Tac_Toe_Game/master/images/TTTicon.png)
+
+#Tic-Tac-Toe
+====
+
+###Summary
+The classic game of X's and O's! Two players take turns marking spaces on a 3x3 grid gameboard. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.  
+
+====
+
+###Technologies Used
+*  HTML5
+*  CSS3
+*  JavaScript
+
+====
+
+###Project Requirements
+
+*  Display an empty tic-tac-toe board when the page is initially displayed.
+*  A player can click on the nine cells to make a move.
+*  Every click will alternate between marking an X and O.
+*  Once occupied with an X or O, the cell cannot be played again.
+*  Provide a Reset Game button that will clear the contents of the board.
+
+====
+Created by Ferdie Quizon
---
+++ js/main.js
@@ -0,0 +1,83 @@
+var player = 1;
+
+function clickBox(box) {
+    if (player ==1) {
+    document.getElementById(box).value = "X";
+    document.getElementById(box).disabled = "disabled";
+    player -=1;
+    winner();
+ } else {
+    document.getElementById(box).value = "O";
+    document.getElementById(box).disabled = "disabled";
+    player +=1;
+    winner();
+ };
+};
+
+function winner (){
+    if(document.getElementById("box1").value === "X" &&
+        document.getElementById("box2").value === "X" &&
+        document.getElementById("box3").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box4").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box6").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box7").value === "X" &&
+        document.getElementById("box8").value === "X" &&
+        document.getElementById("box9").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box1").value === "X" &&
+        document.getElementById("box4").value === "X" &&
+        document.getElementById("box7").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box2").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box8").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box3").value === "X" &&
+        document.getElementById("box6").value === "X" &&
+        document.getElementById("box9").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box1").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box9").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box3").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box7").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box1").value === "O" &&
+        document.getElementById("box2").value === "O" &&
+        document.getElementById("box3").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box4").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box6").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box7").value === "O" &&
+        document.getElementById("box8").value === "O" &&
+        document.getElementById("box9").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box1").value === "O" &&
+        document.getElementById("box4").value === "O" &&
+        document.getElementById("box7").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box2").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box8").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box3").value === "O" &&
+        document.getElementById("box6").value === "O" &&
+        document.getElementById("box9").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box1").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box9").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box3").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box7").value === "O")
+        {alert("Player 2 WINS!");
+    };
+};
