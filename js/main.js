070e89071caec5b53340455f02756cc079f8bf79
Ferdie Quizon <fquizon@gmail.com>
2016-04-18 22:35:30 -0700

    reverted back to button boxes

--- js/main.js
+++ js/main.js
@@ -1,127 +1,98 @@
 var player = 1;
 
-function startGame() {
-    document.turn = "X";
-    setMessage("Player "+ document.turn + " turn!");
-};
-
-
-function setMessage(msg) {
-    document.getElementById("message").innerText = msg;
-};
-
-
-function nextMove(square){
-    square.innerText = document.turn;
-    switchTurn();
-};
-
-function switchTurn() {
-    if (document.turn =="X"){
-        document.turn ="O";
+function switchTurn(){
+    if (document.turn ==="X"){
+        document.turn ==="O";
     } else {
-        document.turn ="X";
+        document.turn ==="X";
     };
+    setMessage("Player "+ document.turn + " turn");
+};
 
-    setMessage("Player "+ document.turn + " turn!");
+function clickBox(box) {
+    if (player ==1) {
+    document.getElementById(box).value = "X";
+    document.getElementById(box).disabled = "disabled";
+    player -=1;
+    winner();
+ } else {
+    document.getElementById(box).value = "O";
+    document.getElementById(box).disabled = "disabled";
+    player +=1;
+    winner();
+ };
 };
 
 
-// var player = 1;
-
-// function switchTurn(){
-//     if (document.turn ==="X"){
-//         document.turn ==="O";
-//     } else {
-//         document.turn ==="X";
-//     };
-//     setMessage("Player "+ document.turn + " turn");
-// };
-
-// function clickBox(box) {
-//     if (player ==1) {
-//     document.getElementById(box).value = "X";
-//     document.getElementById(box).disabled = "disabled";
-//     player -=1;
-//     winner();
-//  } else {
-//     document.getElementById(box).value = "O";
-//     document.getElementById(box).disabled = "disabled";
-//     player +=1;
-//     winner();
-//  };
-// };
 
 
-
-
-// function winner (){
-//     if(document.getElementById("box1").value === "X" &&
-//         document.getElementById("box2").value === "X" &&
-//         document.getElementById("box3").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box4").value === "X" &&
-//         document.getElementById("box5").value === "X" &&
-//         document.getElementById("box6").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box7").value === "X" &&
-//         document.getElementById("box8").value === "X" &&
-//         document.getElementById("box9").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box1").value === "X" &&
-//         document.getElementById("box4").value === "X" &&
-//         document.getElementById("box7").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box2").value === "X" &&
-//         document.getElementById("box5").value === "X" &&
-//         document.getElementById("box8").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box3").value === "X" &&
-//         document.getElementById("box6").value === "X" &&
-//         document.getElementById("box9").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box1").value === "X" &&
-//         document.getElementById("box5").value === "X" &&
-//         document.getElementById("box9").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box3").value === "X" &&
-//         document.getElementById("box5").value === "X" &&
-//         document.getElementById("box7").value === "X")
-//         {alert("Player 1 WINS!");
-//     } if(document.getElementById("box1").value === "O" &&
-//         document.getElementById("box2").value === "O" &&
-//         document.getElementById("box3").value === "O")
-//         {alert("Player 2 WINS!");
-//     } if(document.getElementById("box4").value === "O" &&
-//         document.getElementById("box5").value === "O" &&
-//         document.getElementById("box6").value === "O")
-//         {alert("Player 2 WINS!");
-//     } if(document.getElementById("box7").value === "O" &&
-//         document.getElementById("box8").value === "O" &&
-//         document.getElementById("box9").value === "O")
-//         {alert("Player 2 WINS!");
-//     } if(document.getElementById("box1").value === "O" &&
-//         document.getElementById("box4").value === "O" &&
-//         document.getElementById("box7").value === "O")
-//         {alert("Player 2 WINS!");
-//     } if(document.getElementById("box2").value === "O" &&
-//         document.getElementById("box5").value === "O" &&
-//         document.getElementById("box8").value === "O")
-//         {alert("Player 2 WINS!");
-//     } if(document.getElementById("box3").value === "O" &&
-//         document.getElementById("box6").value === "O" &&
-//         document.getElementById("box9").value === "O")
-//         {alert("Player 2 WINS!");
-//     } if(document.getElementById("box1").value === "O" &&
-//         document.getElementById("box5").value === "O" &&
-//         document.getElementById("box9").value === "O")
-//         {alert("Player 2 WINS!");
-//     } if(document.getElementById("box3").value === "O" &&
-//         document.getElementById("box5").value === "O" &&
-//         document.getElementById("box7").value === "O")
-//         {alert("Player 2 WINS!");
-//     }
-//     else{
-//         alret("Its a Tie! Nobody wins");
-//     };
-// };
+function winner (){
+    if(document.getElementById("box1").value === "X" &&
+        document.getElementById("box2").value === "X" &&
+        document.getElementById("box3").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box4").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box6").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box7").value === "X" &&
+        document.getElementById("box8").value === "X" &&
+        document.getElementById("box9").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box1").value === "X" &&
+        document.getElementById("box4").value === "X" &&
+        document.getElementById("box7").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box2").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box8").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box3").value === "X" &&
+        document.getElementById("box6").value === "X" &&
+        document.getElementById("box9").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box1").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box9").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box3").value === "X" &&
+        document.getElementById("box5").value === "X" &&
+        document.getElementById("box7").value === "X")
+        {alert("Player 1 WINS!");
+    } if(document.getElementById("box1").value === "O" &&
+        document.getElementById("box2").value === "O" &&
+        document.getElementById("box3").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box4").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box6").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box7").value === "O" &&
+        document.getElementById("box8").value === "O" &&
+        document.getElementById("box9").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box1").value === "O" &&
+        document.getElementById("box4").value === "O" &&
+        document.getElementById("box7").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box2").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box8").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box3").value === "O" &&
+        document.getElementById("box6").value === "O" &&
+        document.getElementById("box9").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box1").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box9").value === "O")
+        {alert("Player 2 WINS!");
+    } if(document.getElementById("box3").value === "O" &&
+        document.getElementById("box5").value === "O" &&
+        document.getElementById("box7").value === "O")
+        {alert("Player 2 WINS!");
+    }
+    else{
+        alret("Its a Tie! Nobody wins");
+    };
+};
